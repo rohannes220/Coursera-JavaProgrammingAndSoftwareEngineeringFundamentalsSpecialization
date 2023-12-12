1c4298f92708d6e1763e0b516903fb8c177f8dbd
rohannes220 <rohankumar.id@gmail.com>
2023-12-11 22:32:08 -0800

    Add comments

--- Course1-JavaProgrammingSolvingProblemsWithSoftware/Projects/2-HonorsImageProcessingProject/BatchInversions.java
+++ Course1-JavaProgrammingSolvingProblemsWithSoftware/Projects/2-HonorsImageProcessingProject/BatchInversions.java
@@ -2,27 +2,52 @@ import edu.duke.*;
 import java.io.*;
 
 public class BatchInversions {
+    // given an image return a inverted version of it
     public ImageResource makeInversion(ImageResource inImage) {
+        // we are storing a copy of the input in outiMAGE so that we dont have to find
+        // the x and y or its width and height
         ImageResource outImage = new ImageResource(inImage);
+
+        // we will be returning the outimage since the outimage is basically the inimage
+        // we make changes to its pixelation
+        // to set outimages pixels, we take the pixels from inimage representing by the
+        // for loop and we subtract that from 255
+        // these values are outImage's new pixelations, since outImage is a copy of
+        // inImage we modify inImage's pixels to give us a new image
         for (Pixel p : outImage.pixels()) {
             int newRed = 255 - p.getRed();
             int newBlue = 255 - p.getBlue();
             int newGreen = 255 - p.getGreen();
+
+            // set each pixel to outImage
             p.setRed(newRed);
             p.setBlue(newBlue);
             p.setGreen(newGreen);
         }
+
+        // return the new Image
         return outImage;
     }
 
+    // select some files, convert it to inverted and save it
     public void selectAndConvert() {
+        // use this for multiple files
         DirectoryResource dr = new DirectoryResource();
+        // for every file in the dr
         for (File f : dr.selectedFiles()) {
+            // take the orginialImage store it in inImage
             ImageResource inImage = new ImageResource(f);
+
+            // call the inversion function on inImage
             ImageResource invertedImage = makeInversion(inImage);
+
+            // get the file name and set new file name for the inverted image.
             String fileName = f.getName();
             String invertedFileName = "inverted-" + fileName;
             invertedImage.setFileName(invertedFileName);
+
+            // Since the invertedImage is being called on the inImage, the files will be
+            // saved in that same path
             invertedImage.save();
         }
     }
--- Course1-JavaProgrammingSolvingProblemsWithSoftware/Projects/2-HonorsImageProcessingProject/GrayScale.java
+++ Course1-JavaProgrammingSolvingProblemsWithSoftware/Projects/2-HonorsImageProcessingProject/GrayScale.java
@@ -2,18 +2,24 @@ import edu.duke.*;
 import java.io.*;
 
 public class GrayScale {
+    // take an image and convert it to fully gray
     public ImageResource convertImageToGray(ImageResource inImage) {
-        // ImageResource outImage = new
-        // ImageResource(inImage.getWidth(),inImage.getHeight());
+        // we will modify inImage's pixels to make it gray.
+        // to make the pixel gray, find the average of the red blue and green
+        // set that average for each pixels new values and return image
         for (Pixel inPixel : inImage.pixels()) {
+            // find the average
             int red = inPixel.getRed();
             int green = inPixel.getGreen();
             int blue = inPixel.getBlue();
             int average = (red + green + blue) / 3;
+
+            // set each pixel value to average
             inPixel.setRed(average);
             inPixel.setBlue(average);
             inPixel.setGreen(average);
         }
+        // return the iamge
         return inImage;
     }
 
@@ -23,14 +29,22 @@ public class GrayScale {
         result.draw();
     }
 
+    // take some files and convert the image and save it
     public void batchConvertAndSave() {
+        // hold the files in a directory resource
         DirectoryResource dr = new DirectoryResource();
+        // loop over each file
         for (File f : dr.selectedFiles()) {
+            // store the iamge file in inimage and get the filename
             ImageResource inImage = new ImageResource(f);
             String fileName = f.getName();
 
+            // convert the image to grey and save the file according to the new name rules
             ImageResource grayImage = convertImageToGray(inImage);
             String newGrayFileName = "gray-" + fileName;
+
+            // the file will be saved in the original folder as inimage because the function
+            // is being called on inimage
             grayImage.setFileName(newGrayFileName);
             grayImage.save();
         }
